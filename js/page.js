0f84a3b8bf960103fa9c6019eec0b411aea91d3a
Pedro Henrique Arthur <pedrohma95@gmail.com>
2019-01-01 09:17:15 -0800

    finishing

--- js/page.js
+++ js/page.js
@@ -65,70 +65,76 @@ app.controller('main', function ($scope) {
                         inst: "California Trinity University",
                         date: "Spring 2018 - Present"
                     }
+                },
+                aw:{
+                    text: "Award on the research paper "
                 }
             }
         },
         pt: {
             menu: {
                 first: "sobre",
-                second: "experiencia",
-                third: "educacao",
+                second: "experiência",
+                third: "educação",
                 fourth: "habilidades",
-                fifth: "premios & certificacoes"
+                fifth: "prêmios & certificações"
             },
             section: {
                 about: {
-                    first: "Venho trabalhando exclusivamente como programador e analista de sistemas desde 2014. Todos os meus esforcos sao direcionados ao desenvolvimento, sucesso e excelencia dos meus clientes.",
-                    second: "Eu obtive meu Diploma de Tecnologo em Analise e Desenvolvimento de Sistemas e meu bacharelado em Sistemas da Informacao pela Universidade Presbiteriana Mackenzie. Atualmente estou realizando meu Mestrado na California Trinity University localizada em Los Angeles, USA.",
-                    third: "Ao longo da minha jornada, fui responsavel por organizar varias atividades tais como desenvolvimento de aplicacoes, websites e jogos. Eu amo programar.",
-                    fourth: "Trabalhei em uma das maiores empresas de tecnologia do mundo inteiro, Microsoft, onde aprendi as melhores praticas no desenvolvimento de software, organizacao e professionalismo."
+                    first: "Venho trabalhando exclusivamente como programador e analista de sistemas desde 2014. Todos os meus esforcos são direcionados ao desenvolvimento, sucesso e excelência na entrega do serviço aos meus clientes.",
+                    second: "Eu obtive meu diploma de Tecnólogo em Análise e Desenvolvimento de Sistemas e meu bacharelado em Sistemas da Informação pela Universidade Presbiteriana Mackenzie. Atualmente estou realizando meu mestrado na California Trinity University localizada em Los Angeles, USA.",
+                    third: "Ao longo da minha jornada, fui responsável por organizar varias atividades tais como desenvolvimento de aplicações, websites e jogos. Eu amo programar.",
+                    fourth: "Trabalhei em uma das maiores empresas de tecnologia do mundo inteiro, Microsoft, onde aprendi as melhores praticas no desenvolvimento de software, organização e professionalismo."
                 },
                 exp: {
                     microsoft: {
-                        title: "Estagio em Consultoria",
+                        title: "Estágio em Consultoria",
                         date: "Setembro 2015 – Dezembro 2016",
                         rio: {
-                            text: "Contribui para obter os requerimentos do projeto junto ao time de Analises, compareci a reunioes nao presenciais com o cliente e ajudei a resolver os problemas dele. Desenvolvemos a aplicacao utilizando a IDE do Visual Studio, HTML5, CSS, Bootstrap, jQuery, JavaScript e C#.",
-                            title: "Jogos Olimpicos e Paralimpicos Rio 2016"
+                            text: "Contribui para obter os requerimentos do projeto junto ao time de Análise, compareci a reuniões não presenciais com o cliente e contribui encontrando maneiras para resolver seus problemas. Desenvolvemos a aplicação utilizando a IDE do Visual Studio, HTML5, CSS, Bootstrap, jQuery, JavaScript e C#.",
+                            title: "Jogos Olímpicos e Paralímpicos Rio 2016"
                         },
                         iot: {
-                            text: "Gerenciei um projeto onde desenvolvemos um prototipo de uma casa inteligente, onde os quartos da casa continham sensores (como o de iluminosidade, por exemplo) e eles eram controlados por uma aplicacao universal (Mobile, Tablet, Desktop, Xbox). Todos os dados foram armazenados no Microsoft Azure e replicados para um Stream Analytics onde o mesmo enviava dados para o Power BI que ajudava a ter uma visao maior dos dados. A partir disso tinhamos varios tipo de analise, como o quanto de energia era consumido, e assim por diante.",
+                            text: "Gerenciei um projeto onde desenvolvemos um protótipo de uma casa inteligente, onde os quartos da casa continham sensores (como o de iluminosidade, por exemplo) e eles eram controlados por uma aplicação universal (Mobile, Tablet, Desktop, Xbox). Todos os dados foram armazenados no Microsoft Azure e replicados para um Stream Analytics onde o mesmo enviava dados para o Power BI que ajudava a ter uma visão maior dos dados. A partir disso tinhamos varios tipo de análise, como o quanto de energia era consumida, e assim por diante.",
                             title: "IoT Smarthouse"
                         },
                         ack: {
-                            text: "Contribui no desenvolvimento de uma Ack Tool para o time de Compliance, utilizando a arquitetura do SharePoitn, onde eles conseguem coletar assinaturas dos Gerentes e Leads da area. Nos utilizamos os servicos do SharePoint junto com o InfoPath para criar os formularios.",
+                            text: "Contribui no desenvolvimento de uma Ack Tool para o time de Compliance, utilizando a arquitetura do SharePoitn, onde eles conseguem coletar assinaturas dos Gerentes e Leads da area. Nós utilizamos os servicos do SharePoint junto com o InfoPath para criar os formularios.",
                             title: "Ack Tool"
                         }
                     },
                     brq: {
-                        title: "Estagio em Programacao",
+                        title: "Estágio em Programação",
                         date: "Maio 2014 – Maio 2015",
                         itau: {
-                            text: "Contribui para a atualizacao de uma aplicacao que estava utilizando servidor WebLogic 8 para um WebLogic 11. Trabalhei com a documentacao do sistema, onde era utilizado a linguagem de programacao Vb.Net e todos os tipos de diagrama foram criados.",
+                            text: "Contribui para a atualização de uma aplicação que estava utilizando servidor WebLogic 8 para um WebLogic 11. Trabalhei com a documentação do sistema, onde era utilizado a linguagem de programação Vb.Net e todos os tipos de diagrama foram criados.",
                             title: "Banco Itaú"
                         },
                         cielo: {
-                            text: "Desenvolvemos um sistema que ajuda os clientes Cielo a criar suas proprias queries. Esse software permite que os vendedores atraves dessa aplicacao consultem suas vendas, o quanto tem a receber, e muitas outras informacoes em um so lugar. Nos utilizamos Java EE, Spring MVC, JSF, Primefaces, Thymeleaf, Bootstrap, WebLogic, WebSphere and IBM Portal. ",
+                            text: "Desenvolvemos um sistema que ajuda os clientes Cielo a criar suas próprias queries. Esse software permite que os vendedores consultem suas vendas, o quanto tem a receber, e muitas outras informações em um só lugar. Nós utilizamos Java EE, Spring MVC, JSF, Primefaces, Thymeleaf, Bootstrap, WebLogic, WebSphere and IBM Portal. ",
                             title: "Cielo"
                         }
                     }
                 },
                 edu: {
                     ass: {
-                        title: "Tecnologia em Analise e Desenvolvimento de Sistemas",
+                        title: "Tecnologia em Análise e Desenvolvimento de Sistemas",
                         inst: "Universidade Presbiteriana Mackenzie",
                         date: "Janeiro 2013 - Julho 2015"
                     },
                     bach: {
-                        title: "Sistemas da Informacao",
+                        title: "Sistemas da Informação",
                         inst: "Universidade Presbiteriana Mackenzie",
                         date: "Janeiro 2013 - Dezembro 2016"
                     },
                     mba: {
-                        title: "Mestrado em Sistemas da Informacao",
+                        title: "Mestrado em Sistemas da Informação",
                         inst: "California Trinity University",
                         date: "January 2017 - Cursando"
                     }
+                },
+                aw:{
+                    text: "Prêmio pela pesquisa desenvolvida "
                 }
             }
         }
