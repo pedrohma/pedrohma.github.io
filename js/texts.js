07ebdead54d50b63e598e39416f0649a1e638bb9
Pedro Henrique de Miranda Arthur <pedrohma95@gmail.com>
2020-01-30 15:19:22 -0800

    adding few more ideas

--- js/texts.js
+++ js/texts.js
@@ -9,10 +9,9 @@ const locale = {
         },
         section: {
             about: {
-                first: "I have worked exclusively as a programmer and systems analyst since 2014. All my efforts are directed towards the development, success and excellence of my clients.",
-                second: "I earned my associate degree in Technology and Analysis and Development of Systems and my bachelor’s degree in Systems Information at Mackenzie Presbyterian University in São Paulo, Brazil. Now I'm taking my MSI at California Trinity University in Los Angeles, USA.",
-                third: "Throughout my life, I've been the responsible of organizing various activities such as developing applications, websites and games. My passion and purpose is programming.",
-                fourth: "Working at one of the world's largest technology companies, Microsoft , taught me best practices in software development, as well organization, planning, and professionalism."
+                first: "I have worked exclusively as a programmer and systems analyst since 2014. All my efforts are directed towards the development, success and excellence of my projects.",
+                second: "I earned my associate degree in Technology and Analysis and Development of Systems, my bachelor’s degree in Systems Information at Mackenzie Presbyterian University in São Paulo, Brazil and I just got my degree for MBA with concentration in Los Angeles, USA.",
+                third: "I love music, cooking and arts in general. What would be life without it?",
             },
             exp: {
                 microsoft: {
@@ -154,7 +153,7 @@ const locale = {
             },
             aw:{
                 text: "Prêmio pela pesquisa desenvolvida ",
-                vp: "Vice Presidente do comitê de estudantes da "
+                vp: "Vice Presidente do comitê de estudantes em "
             }
         }
     }
