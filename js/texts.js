4a8d18789b855bbeeff91c7e74f3e873960e4b77
Pedro Henrique de Miranda Arthur <pedrohma95@gmail.com>
2020-01-30 17:08:51 -0800

    adding ideas

--- js/texts.js
+++ js/texts.js
@@ -10,24 +10,23 @@ const locale = {
         section: {
             about: {
                 first: "I have worked exclusively as a programmer and systems analyst since 2014. All my efforts are directed towards the development, success and excellence of my projects.",
-                second: "I earned my associate degree in Technology and Analysis and Development of Systems, my bachelor’s degree in Systems Information at Mackenzie Presbyterian University in São Paulo, Brazil and I just got my degree for MBA with concentration in Los Angeles, USA.",
-                third: "I love music, cooking and arts in general. What would be life without it?",
+                second: "I earned my associate degree in Technology and Analysis and Development of Systems, my bachelor’s degree in Systems Information at Mackenzie Presbyterian University in São Paulo, Brazil and I just got my MBA degree in Los Angeles, USA.",
+                third: "I love working out, music, cooking, arts and reading.",
             },
             exp: {
                 microsoft: {
                     title: "CONSULTANT INTERNSHIP",
                     date: "Sep 2015 – Dez 2016",
                     rio: {
-                        text: "Contributed to get system requirement with the Analysis team, went to non-face-to-face meetings with the customer, and solved the customer's issues. Developed an application using Visual Studio as IDE, HTML5, CSS, Bootstrap, jQuery, JavaScript and C#.",
+                        text: "",
                         title: "Rio 2016 Olypmic and Paralympic Games"
                     },
                     iot: {
-                        text: "Managed a project with the development of a prototype of an intelligent house, where the rooms of the house have sensors (such as luminosity, for example), and are controlled through a universal application (Mobile, Tablet, Desktop, Xbox). All data is stored on the Microsoft Azure cloud computing platform and replicated to a Stream Analytics where it sends the data to a Power BI which improved analysis of data. From this we can do various types of analysis, such as how much energy is consumed, and so on.",
+                        text: "",
                         title: "IoT Smarthouse"
-                    }
-                    ,
+                    },
                     ack: {
-                        text: "Developed an Ack Tool for the Compliance team, under the architecture of SharePoint, where they can collect signatures of the data Managers and Leads of the area. We use SharePoint services together with InfoPath to create forms.",
+                        text: "",
                         title: "Ack Tool"
                     }
                 },
@@ -35,11 +34,11 @@ const locale = {
                     title: "PROGRAMMER INTERNSHIP",
                     date: "May 2014 – May 2015",
                     itau: {
-                        text: "Contributed with the upgrade of the application server that was a WebLogic 8 to a WebLogic 11. Worked with the documentation of the same, which used Vb.Net as its programming language, all types of diagram were developed.",
+                        text: "",
                         title: "Itaú Bank"
                     },
                     cielo: {
-                        text: "Developed a system that allows Cielo users to make their own queries. This software allows sellers through this application to consult their sales, amounts receivable, their outstanding finances, etc. all in one place. We used Java EE development, Spring MVC, JSF, Primefaces, Thymeleaf, Bootstrap, WebLogic, WebSphere and IBM Portal.",
+                        text: "",
                         title: "Cielo"
                     }
                 }
@@ -61,7 +60,7 @@ const locale = {
                     date: "Spring 2018 - Winter 2019"
                 }
             },
-            skills:{
+            skills: {
                 ver: "Versioning Tools",
                 pl: "Programming Languages",
                 wd: "Web Development",
@@ -72,7 +71,7 @@ const locale = {
                 cd: "Cross Browser Testing & Debugging",
                 cf: "Cross Functional Teams"
             },
-            aw:{
+            aw: {
                 text: "Award on the research paper ",
                 vp: "Vice President of the student committee at "
             }
@@ -98,15 +97,15 @@ const locale = {
                     title: "Estágio em Consultoria",
                     date: "Setembro 2015 – Dezembro 2016",
                     rio: {
-                        text: "Contribui para obter os requerimentos do projeto junto ao time de Análise, compareci a reuniões não presenciais com o cliente e contribui encontrando maneiras para resolver seus problemas. Desenvolvemos a aplicação utilizando a IDE do Visual Studio, HTML5, CSS, Bootstrap, jQuery, JavaScript e C#.",
+                        text: "",
                         title: "Jogos Olímpicos e Paralímpicos Rio 2016"
                     },
                     iot: {
-                        text: "Gerenciei um projeto onde desenvolvemos um protótipo de uma casa inteligente, onde os quartos da casa continham sensores (como o de iluminosidade, por exemplo) e eles eram controlados por uma aplicação universal (Mobile, Tablet, Desktop, Xbox). Todos os dados foram armazenados no Microsoft Azure e replicados para um Stream Analytics onde o mesmo enviava dados para o Power BI que ajudava a ter uma visão maior dos dados. A partir disso tinhamos varios tipo de análise, como o quanto de energia era consumida, e assim por diante.",
+                        text: "",
                         title: "IoT Smarthouse"
                     },
                     ack: {
-                        text: "Contribui no desenvolvimento de uma Ack Tool para o time de Compliance, utilizando a arquitetura do SharePoitn, onde eles conseguem coletar assinaturas dos Gerentes e Leads da area. Nós utilizamos os servicos do SharePoint junto com o InfoPath para criar os formularios.",
+                        text: "",
                         title: "Ack Tool"
                     }
                 },
@@ -114,11 +113,11 @@ const locale = {
                     title: "Estágio em Programação",
                     date: "Maio 2014 – Maio 2015",
                     itau: {
-                        text: "Contribui para a atualização de uma aplicação que estava utilizando servidor WebLogic 8 para um WebLogic 11. Trabalhei com a documentação do sistema, onde era utilizado a linguagem de programação Vb.Net e todos os tipos de diagrama foram criados.",
+                        text: "",
                         title: "Banco Itaú"
                     },
                     cielo: {
-                        text: "Desenvolvemos um sistema que ajuda os clientes Cielo a criar suas próprias queries. Esse software permite que os vendedores consultem suas vendas, o quanto tem a receber, e muitas outras informações em um só lugar. Nós utilizamos Java EE, Spring MVC, JSF, Primefaces, Thymeleaf, Bootstrap, WebLogic, WebSphere and IBM Portal. ",
+                        text: "",
                         title: "Cielo"
                     }
                 }
@@ -140,7 +139,7 @@ const locale = {
                     date: "Março 2018 - Dezembro 2019"
                 }
             },
-            skills:{
+            skills: {
                 ver: "Ferramentas de Versionamento",
                 pl: "Linguagens de Programação",
                 wd: "Desenvolvimento Web",
@@ -151,7 +150,7 @@ const locale = {
                 cd: "Testes & Debugging em diferentes tipos de browsers",
                 cf: "Equipes multifuncionais"
             },
-            aw:{
+            aw: {
                 text: "Prêmio pela pesquisa desenvolvida ",
                 vp: "Vice Presidente do comitê de estudantes em "
             }
